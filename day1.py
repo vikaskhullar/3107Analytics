10bc2ce1d6df256acade8f9cdbd5bd50f27fbf19
vikaskhullar <vikas.khullar@gmail.com>
2022-07-31 22:38:15 +0530

    update

---
+++ day1.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 31 21:15:59 2022
+
+@author: vikas
+"""
+
+
+
+#String Data type
+
+"Hi welcome to python"
+
+"Hi's"
+
+'Hi welcome to python'
+    
+print("hi")
+
+print("Hi")
+
+print?
+
+print("Hi", "welocme", "to", "Python")
+
+print("Hi", "welocme", "to", "Python", sep="-")
+
+
+
+print("Hi")
+print("Welcome")
+print("To")
+print("Python")
+
+
+"Hi welcome \n to python"
+
+
+print("Hi", end="-")
+print("Welcome", end="-")
+print("To")
+print("Python")
+
+
+import pandas
+
+pandas.__version__
+
+
+import pandas as pd
+
+pd.__version__
+pd.__version__
+
+
+
+country = "India"
+
+
+print("I live in India")
+
+print("I live in country")
+
+print("I live in", country)
+
+print(country)
+
+print("I live in", country, sep='-')
+
+print(f"I live in {country}", sep='-')
+
+
+f"I live in {country}"
+
+
+print("I live in {0}".format(country))
+
+
+
+# Static time Data variable
+
+lc = 'India'
+wc = 'USA'
+
+print(f"I live in {lc} and I work for {wc}")
+
+print("I live in {1} and I work for {0}".format(lc,wc))
+
+
+
+
+# Static time Data variable
+
+lc = input("Enter Live Country")
+wc = input("Enter Work Country")
+
+print(f"I live in {lc} and I work for {wc}")
+
+print("I live in {1} and I work for {0}".format(lc,wc))
+
+
+
+
+#
+
+
+c = "V" + "K"
+c
+
+c= 10 + 20
+c
+
+
+
+c = "Marks are " + 50
+
+# Apply Type Conversion  or Type Conversion
+
+c = "Marks are " + str(50)
+c
+
+
+s = '10'
+
+i = int(s)
+
+print(i)
+
+type(i)
+
+type(s)
+
+
+
+
+
+print()?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
